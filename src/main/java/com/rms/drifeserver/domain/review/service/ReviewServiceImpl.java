f42183b2f6502ccc729798ecc1fd08aae8ee3312
pjoon357 <pjoon357@naver.com>
2022-02-27 17:45:32 +0900

    feat: 리뷰 수정/삭제 controller 추가

--- src/main/java/com/rms/drifeserver/domain/review/service/ReviewServiceImpl.java
+++ src/main/java/com/rms/drifeserver/domain/review/service/ReviewServiceImpl.java
@@ -1,6 +1,7 @@
 package com.rms.drifeserver.domain.review.service;
 
 import com.rms.drifeserver.domain.review.dao.ReviewRepository;
+import com.rms.drifeserver.domain.review.service.dto.request.UpdateReviewRequest;
 import com.rms.drifeserver.domain.review.service.dto.response.ReviewDetailResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,17 @@ public class ReviewServiceImpl implements ReviewService{
     private final ReviewRepository reviewRepository;
 
     @Override
-    public ReviewDetailResponse getReviewDetail(Long userId, Long storeId, Long reviewId) {
+    public ReviewDetailResponse getReviewDetail( Long storeId, Long reviewId) {
         return null;
+    }
+
+    @Override
+    public ReviewDetailResponse updateReview(UpdateReviewRequest request, Long storeId, Long reviewId) {
+        return null;
+    }
+
+    @Override
+    public void deleteReview(Long storeId, Long reviewId) {
+
     }
 }
