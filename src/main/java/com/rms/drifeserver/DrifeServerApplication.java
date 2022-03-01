ee74ea4259c8fc9e1418a7ec28c0799366492fda
taesung <taesung320@gmail.com>
2022-03-01 17:51:38 +0900

    social login with spring security

--- src/main/java/com/rms/drifeserver/DrifeServerApplication.java
+++ src/main/java/com/rms/drifeserver/DrifeServerApplication.java
@@ -1,9 +1,16 @@
 package com.rms.drifeserver;
 
+import com.rms.drifeserver.domain.oauthlogin.config.properties.AppProperties;
+import com.rms.drifeserver.domain.oauthlogin.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		CorsProperties.class,
+		AppProperties.class
+})
 public class DrifeServerApplication {
 
 	public static void main(String[] args) {
