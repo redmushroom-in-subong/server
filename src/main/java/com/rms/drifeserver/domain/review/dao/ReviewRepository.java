f1da1ab3671bc195cf038c3e9d9a4eeef6c85351
pjoon357 <pjoon357@naver.com>
2022-03-13 18:48:21 +0900

    feat: 리뷰 목록 조회 서비스 및 테스트 추가

--- src/main/java/com/rms/drifeserver/domain/review/dao/ReviewRepository.java
+++ src/main/java/com/rms/drifeserver/domain/review/dao/ReviewRepository.java
@@ -3,9 +3,19 @@ package com.rms.drifeserver.domain.review.dao;
 import com.rms.drifeserver.domain.review.model.Review;
 import com.rms.drifeserver.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Optional<Review> findByIdAndUserId(Long id, Long userId);
+
+    List<Review> findAllByStoreId(Long storeId);
+
+    List<Review> findAllByStoreIdAndUserId(Long storeId, Long userId);
+
+    @Query("select r from Review r left join ReviewKeyword rk on ( r.id = rk.review.id ) where ( r.store.id = :storeId and rk.reviewKeywordType.id = :keywordTypeId )")
+    List<Review> findAllByStoreIdAndKeywordTypeId(@Param("storeId") Long storeId, @Param("keywordTypeId") Long keywordTypeId);
 }
--- src/main/java/com/rms/drifeserver/domain/review/service/VisitServiceImpl.java
+++ src/main/java/com/rms/drifeserver/domain/review/service/VisitServiceImpl.java
@@ -1,17 +1,33 @@
 package com.rms.drifeserver.domain.review.service;
 
+import com.rms.drifeserver.domain.common.exception.BaseException;
+import com.rms.drifeserver.domain.common.exception.type.ErrorCode;
 import com.rms.drifeserver.domain.review.dao.VisitRepository;
+import com.rms.drifeserver.domain.review.model.Review;
+import com.rms.drifeserver.domain.review.model.Visit;
+import com.rms.drifeserver.domain.store.dao.StoreRepository;
+import com.rms.drifeserver.domain.store.model.Store;
+import com.rms.drifeserver.domain.user.dao.UserRepository;
+import com.rms.drifeserver.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
 @RequiredArgsConstructor
 public class VisitServiceImpl implements VisitService {
     private final VisitRepository visitRepository;
+    private final UserRepository userRepository;
+    private final StoreRepository storeRepository;
 
+    @Transactional
     @Override
     public void addVisit(Long userId, Long storeId) {
+        User user = userRepository.getById(userId);
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STORE));
 
+        visitRepository.save(Visit.of(user, store));
     }
 }
