eba7ba932272ef912c6702f9d55bc30da8f45821
pjoon357 <pjoon357@naver.com>
2022-03-17 19:12:31 +0900

    feat: 가게별 유저 티어 추가

--- src/main/java/com/rms/drifeserver/domain/review/service/ReviewServiceUtils.java
+++ src/main/java/com/rms/drifeserver/domain/review/service/ReviewServiceUtils.java
@@ -4,8 +4,6 @@ import com.rms.drifeserver.domain.review.dao.ReviewRepository;
 import com.rms.drifeserver.domain.review.dao.VisitRepository;
 import com.rms.drifeserver.domain.review.model.Review;
 import com.rms.drifeserver.domain.review.service.dto.response.ReviewCounterResponse;
-import com.rms.drifeserver.domain.store.model.Store;
-import com.rms.drifeserver.domain.user.model.User;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
@@ -14,16 +12,30 @@ import javax.validation.constraints.NotNull;
 public class ReviewServiceUtils {
 
     @NotNull
-    static ReviewCounterResponse findCount(VisitRepository visitRepository, ReviewRepository reviewRepository, Review review) {
+    public static ReviewCounterResponse findCount(VisitRepository visitRepository, ReviewRepository reviewRepository, Review review) {
 
         long storeVisitCount = visitRepository.countByStore(review.getStore());
         long storeReviewCount = visitRepository.countByStoreWithCustom(review.getStore());
         long storeCustomCount = reviewRepository.countByStore(review.getStore());
         long myVisitCount = visitRepository.countByStoreAndUser(review.getStore(), review.getUser());
         long myReviewCount = reviewRepository.countByStoreAndUser(review.getStore(), review.getUser());
-        String myStoreTier = "";
+        String myStoreTier = getMyStoreTierByCount(myVisitCount);
 
         return ReviewCounterResponse.of(storeVisitCount, storeReviewCount, storeCustomCount, myVisitCount, myReviewCount, myStoreTier);
+    }
+
+    private static String getMyStoreTierByCount(long count) {
+
+        if (count >= 20) {
+            return "소울메이트";
+        } else if (count >= 10) {
+            return "절친한 사이";
+        } else if (count >= 5) {
+            return "친한 사이";
+        } else if (count >= 1) {
+            return "어색한 사이";
+        }
+        return "모르는 사이";
     }
 
 }
