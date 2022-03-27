e30b3c31e577da7b26f7c079317c338454b65d0c
pjoon357 <pjoon357@naver.com>
2022-03-27 19:45:18 +0900

    fix: 리뷰 엔티티 유저 접근제어자 protect로 수정

--- src/main/java/com/rms/drifeserver/domain/review/service/dto/response/ReviewDetailResponse.java
+++ src/main/java/com/rms/drifeserver/domain/review/service/dto/response/ReviewDetailResponse.java
@@ -10,7 +10,6 @@ import org.springframework.lang.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
