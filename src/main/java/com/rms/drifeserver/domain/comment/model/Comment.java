5bfe6ada24c2b154d64b22d81cfeedb39b7a3694
pjoon357 <pjoon357@naver.com>
2022-04-02 02:07:30 +0900

    feat: 댓글 서비스 추가

--- src/main/java/com/rms/drifeserver/domain/comment/model/Comment.java
+++ src/main/java/com/rms/drifeserver/domain/comment/model/Comment.java
@@ -2,7 +2,11 @@ package com.rms.drifeserver.domain.comment.model;
 
 import com.rms.drifeserver.domain.board.model.Board;
 import com.rms.drifeserver.domain.common.model.BaseTimeEntity;
+import com.rms.drifeserver.domain.user.model.User;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -11,6 +15,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +25,23 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
     private String contents;
 
+    private Comment(Board board, User user, String contents) {
+        this.board = board;
+        this.user = user;
+        this.contents = contents;
+    }
+
+    public static Comment of(Board board, User user, String contents) {
+        return new Comment(board, user, contents);
+    }
+
+    public void update(String contents) {
+        this.contents = contents;
+    }
 }
