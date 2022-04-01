794fabd7963c8bf6364207e08bf43c2a215283a2
pjoon357 <pjoon357@naver.com>
2022-04-02 02:07:30 +0900

    feat: 댓글 서비스 추가

--- src/main/java/com/rms/drifeserver/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/rms/drifeserver/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package com.rms.drifeserver.domain.comment.repository;
 
 import com.rms.drifeserver.domain.comment.model.Comment;
+import com.rms.drifeserver.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Optional<Comment> findByIdAndUser(Long id, User user);
 }
--- src/main/java/com/rms/drifeserver/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/com/rms/drifeserver/domain/comment/service/CommentServiceImpl.java
@@ -1,27 +1,52 @@
 package com.rms.drifeserver.domain.comment.service;
 
+import com.rms.drifeserver.domain.board.model.Board;
+import com.rms.drifeserver.domain.board.repository.BoardRepository;
+import com.rms.drifeserver.domain.comment.model.Comment;
+import com.rms.drifeserver.domain.comment.repository.CommentRepository;
 import com.rms.drifeserver.domain.comment.service.dto.request.AddCommentRequest;
 import com.rms.drifeserver.domain.comment.service.dto.request.UpdateCommentRequest;
 import com.rms.drifeserver.domain.comment.service.dto.response.CommentInfoResponse;
+import com.rms.drifeserver.domain.common.exception.BaseException;
+import com.rms.drifeserver.domain.common.exception.type.ErrorCode;
 import com.rms.drifeserver.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class CommentServiceImpl implements CommentService{
+public class CommentServiceImpl implements CommentService {
+
+    private final BoardRepository boardRepository;
+    private final CommentRepository commentRepository;
+
+    @Transactional
     @Override
     public void addComment(AddCommentRequest request, Long boardId, User user) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_BOARD));
 
+        Comment comment = request.toComment(board, user);
+
+        commentRepository.save(comment);
     }
 
     @Override
     public CommentInfoResponse updateComment(UpdateCommentRequest request, Long commentId, User user) {
-        return null;
+        Comment comment = commentRepository.findByIdAndUser(commentId, user)
+                .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_COMMENT));
+
+        comment.update(request.getContents());
+
+        return CommentInfoResponse.of(comment, 0L);
     }
 
     @Override
     public void deleteComment(Long commentId, User user) {
+        Comment comment = commentRepository.findByIdAndUser(commentId, user)
+                .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_COMMENT));
 
+        commentRepository.delete(comment);
     }
 }
