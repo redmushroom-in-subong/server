d237049490759f95dcac29b43bf19c24db9f1b2e
pjoon357 <pjoon357@naver.com>
2022-04-02 02:35:09 +0900

    feat: 댓글 좋아요 기능 추가

--- src/main/java/com/rms/drifeserver/domain/like/dao/CommentLikesRepository.java
+++ src/main/java/com/rms/drifeserver/domain/like/dao/CommentLikesRepository.java
@@ -1,7 +1,14 @@
 package com.rms.drifeserver.domain.like.dao;
 
+import com.rms.drifeserver.domain.comment.model.Comment;
 import com.rms.drifeserver.domain.like.model.CommentLikes;
+import com.rms.drifeserver.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentLikesRepository extends JpaRepository<CommentLikes,Long> {
+public interface CommentLikesRepository extends JpaRepository<CommentLikes, Long> {
+
+    Long countByComment(Comment comment);
+
+    CommentLikes findByUserAndComment(User user, Comment comment);
+
 }
