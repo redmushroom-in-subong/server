10e90cbcfb53d63184c1c5015a8c61b56bdda2f6
lse99 <nifer0@naver.com>
2022-04-09 17:46:55 +0900

    feat: 해당 가게 좋아요 추가, 해당 가게에 대한 유저 정보 조회 수정

--- src/main/java/com/rms/drifeserver/domain/like/model/StoreLikes.java
+++ src/main/java/com/rms/drifeserver/domain/like/model/StoreLikes.java
@@ -1,6 +1,7 @@
 package com.rms.drifeserver.domain.like.model;
 
 import com.rms.drifeserver.domain.store.model.Store;
+import com.rms.drifeserver.domain.user.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +13,18 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Setter
 @DiscriminatorValue(value = "store")
 public class StoreLikes extends Like {
     @ManyToOne
     @JoinColumn(name = "store_id")
     private Store store;
+
+    public StoreLikes(User user, Store store) {
+        this.user = user;
+        this.store = store;
+    }
+
+    public static StoreLikes of(User user, Store store) {
+        return new StoreLikes(user, store);
+    }
 }
