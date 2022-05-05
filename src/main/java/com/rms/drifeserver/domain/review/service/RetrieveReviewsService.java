e73ee5d93fbca3aac87be9b35dfaee56d9aef85e
pjoon357 <pjoon357@naver.com>
2022-05-06 01:12:59 +0900

    feat: 리뷰 목록 조회 페이징 추가

--- src/main/java/com/rms/drifeserver/domain/review/service/RetrieveReviewsService.java
+++ src/main/java/com/rms/drifeserver/domain/review/service/RetrieveReviewsService.java
@@ -1,9 +1,10 @@
 package com.rms.drifeserver.domain.review.service;
 
 import com.rms.drifeserver.domain.review.service.dto.response.ReviewsResponse;
+import org.springframework.data.domain.Pageable;
 
 public interface RetrieveReviewsService {
-    ReviewsResponse getAllReviewsInStore(Long storeId);
-    ReviewsResponse getAllReviewsInStoreWithUserId(Long storeId, Long userId);
-    ReviewsResponse getAllReviewsInStoreWithKeywordId(Long storeId, Long keywordId);
+    ReviewsResponse getAllReviewsInStore(Long storeId, Pageable pageable);
+    ReviewsResponse getAllReviewsInStoreWithUserId(Long storeId, Long userId, Pageable pageable);
+    ReviewsResponse getAllReviewsInStoreWithKeywordId(Long storeId, Long keywordId, Pageable pageable);
 }
