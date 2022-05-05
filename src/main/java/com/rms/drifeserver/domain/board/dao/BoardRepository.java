82f6f1b29d4b58fd93b57c3d4e688493ef65746a
pjoon357 <pjoon357@naver.com>
2022-05-06 00:55:46 +0900

    feat: 게시판 목록 조회 페이징 추가

--- src/main/java/com/rms/drifeserver/domain/board/dao/BoardRepository.java
+++ src/main/java/com/rms/drifeserver/domain/board/dao/BoardRepository.java
@@ -2,6 +2,7 @@ package com.rms.drifeserver.domain.board.dao;
 
 import com.rms.drifeserver.domain.board.model.Board;
 import com.rms.drifeserver.domain.user.model.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,8 +14,8 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Optional<Board> findByIdAndUser(Long boardId, User user);
 
-    List<Board> findByCodeOrderByCreatedAtDesc(String code);
+    List<Board> findByCodeOrderByCreatedAtDesc(String code, Pageable pageable);
 
     @Query("select b from BoardLikes bl join Board b on (b.code = :code) group by b having count(b) >=10")
-    List<Board> findHotBoards(@Param("code") String code);
+    List<Board> findHotBoards(@Param("code") String code, Pageable pageable);
 }
