45902a8708da53fdc82f8c338ae140b2dc5e206f
taesung320 <taesung320@gmail.com>
2022-05-10 17:21:06 +0900

    feat : transactional async eventlister

--- src/main/java/com/rms/drifeserver/domain/badge/event/UserBadgeEventHandler.java
+++ src/main/java/com/rms/drifeserver/domain/badge/event/UserBadgeEventHandler.java
@@ -4,19 +4,23 @@ import com.rms.drifeserver.domain.badge.service.BadgeService;
 import com.rms.drifeserver.domain.common.exception.BaseException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import javax.transaction.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class UserBadgeEventHandler {
     private final BadgeService badgeService;
 
-    @EventListener
+    @TransactionalEventListener
+    @Async
     public void onApplicationEvent(UserBadgeEvent event) throws Exception, BaseException {
-        System.out.println("check user badge event  ");
         badgeService.checkBadgeEarnCondition(event.getUserId());
     }
-
 }
