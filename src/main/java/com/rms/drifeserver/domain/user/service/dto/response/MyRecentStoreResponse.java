7bc40541808914938f47622ce147b5a7d7a5b699
taesung320 <taesung320@gmail.com>
2022-05-12 22:19:03 +0900

    temp : 중간저장 - 쿼리문제 발생

--- src/main/java/com/rms/drifeserver/domain/user/service/dto/response/MyRecentStoreResponse.java
+++ src/main/java/com/rms/drifeserver/domain/user/service/dto/response/MyRecentStoreResponse.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 //@AllArgsConstructor
 @NoArgsConstructor
@@ -22,14 +23,30 @@ public class MyRecentStoreResponse {
     private int visitCount;
     private boolean isLike;
 
-    public MyRecentStoreResponse(Long visitId,Long storeId, String storeName, LocalDateTime visitedAt, String thumbnailImage, long regularCustomerCount, int visitCount, boolean isLike) {
+    public MyRecentStoreResponse(Long visitId,Long storeId, String storeName, String visitedAt, String thumbnailImage, long regularCustomerCount, int visitCount, boolean isLike) {
         this.visitId = visitId;
         this.storeId = storeId;
         this.storeName = storeName;
-        this.visitedAt = visitedAt.toString().substring(0,10);
+        this.visitedAt = visitedAt;
         this.thumbnailImage = thumbnailImage;
         this.regularCustomerCount = regularCustomerCount;
         this.visitCount = visitCount;
         this.isLike = isLike;
+    }
+
+    static public MyRecentStoreResponse of(Long visitId,Long storeId, String storeName, String visitedAt,String thumbnailImage, long regularCustomerCount, int visitCount, boolean isLike) {
+        return new MyRecentStoreResponse( visitId,storeId,  storeName, visitedAt, thumbnailImage ,regularCustomerCount,  visitCount,  isLike);
+    }
+    static public MyRecentStoreResponse of(Map<String,Object> resultMap){
+        return new MyRecentStoreResponse(
+                Long.parseLong(resultMap.get("visitedId").toString()),
+               Long.parseLong(resultMap.get("storeId").toString()),
+                resultMap.get("storeName").toString(),
+                resultMap.get("visitedAt").toString(),
+                resultMap.get("thumbnailImage").toString(),
+                Long.parseLong(resultMap.get("regularCustomerCount").toString()),
+                Integer.parseInt(resultMap.get("visitCount").toString()),
+                Boolean.parseBoolean(resultMap.get("isLike").toString())
+        );
     }
 }
