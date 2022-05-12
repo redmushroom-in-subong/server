81b7998b17be5cd9562949589bd11365456f2a0d
taesung320 <taesung320@gmail.com>
2022-05-12 23:51:21 +0900

    feat : 나의 최근 방문가게, 나의 찜한가게 API

--- src/main/java/com/rms/drifeserver/domain/user/dao/MyStoreRepository.java
+++ src/main/java/com/rms/drifeserver/domain/user/dao/MyStoreRepository.java
@@ -1,6 +1,7 @@
 package com.rms.drifeserver.domain.user.dao;
 
 import com.rms.drifeserver.domain.user.model.User;
+import com.rms.drifeserver.domain.user.service.dto.response.MyRecentStoreResponse;
 import com.rms.drifeserver.domain.user.service.dto.response.MyStoreResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,25 +17,29 @@ public interface MyStoreRepository extends JpaRepository<User, Long> {
     //TODO 단골 손님에 대한 기준, 현재 쿼리로 알아내야 할 지 이벤트 기반으로 단골 손님을 넣어야 할지
     // - 단골수가 자주 쓰이기 때문에 따로저장이 맞을것같음
     //TODO store에 thumbnail 필드 필요
-    @Query("select distinct new com.rms.drifeserver.domain.user.service.dto.response.MyStoreResponse(" +
+    @Query("select distinct new com.rms.drifeserver.domain.user.service.dto.response.MyRecentStoreResponse(" +
             "s.id," +
             "s.storeName," +
-            "'empty',"+
+            "max(v.updatedAt)," +
+            "'empty', " +
             "(select count(user) from User user where 10<(select count(u) from Visit v join v.user u where v.store.id=s.id ) )," +
             "s.visits.size," +
+            "s.reviews.size," +
             "(l.store is not null) )" +
             "from Store s join s.visits v left join s.likes l " +
-//            "group by v.store" +
-            "where v.user.id=:userId order by v.updatedAt")
-    List<MyStoreResponse> findRecentStore(@Param("userId") Long userId);
+            "where v.user.id=:userId " +
+            "group by s"
+            )
+    List<MyRecentStoreResponse> findRecentStore(@Param("userId") Long userId);
     @Query("select u from User u ")
     List<MyStoreResponse> findFrequentStore(@Param("userId") Long userId);
     @Query("select distinct new com.rms.drifeserver.domain.user.service.dto.response.MyStoreResponse(" +
             "s.id," +
             "s.storeName," +
-            "'empty',"+
+            "'empty', " +
             "(select count(user) from User user where 10<(select count(u) from Visit v join v.user u where v.store.id=s.id ) )," +
             "s.visits.size," +
+            "s.reviews.size," +
             "(l.store is not null) )" +
             "from Store s join s.likes l   " +
             "where l.user.id=:userId ")
--- src/main/java/com/rms/drifeserver/domain/user/service/dto/response/MyStoreResponse.java
+++ src/main/java/com/rms/drifeserver/domain/user/service/dto/response/MyStoreResponse.java
@@ -2,9 +2,8 @@ package com.rms.drifeserver.domain.user.service.dto.response;
 
 import lombok.*;
 
-import java.time.LocalDateTime;
 
-//@AllArgsConstructor
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
@@ -12,31 +11,9 @@ import java.time.LocalDateTime;
 public class MyStoreResponse {
     private Long storeId;
     private String storeName;
-    private String visitedAt;
     private String thumbnailImage;
-    private long regularCustomerCount;
+    private Long regularCustomerCount;
     private int visitCount;
+    private int reviewCount;
     private boolean isLike;
-
-    public MyStoreResponse(Long storeId, String storeName, LocalDateTime visitedAt, String thumbnailImage, long regularCustomerCount, int visitCount, boolean isLike) {
-        this.storeId = storeId;
-        this.storeName = storeName;
-        this.visitedAt = visitedAt.toString().substring(0,10);
-        this.thumbnailImage = thumbnailImage;
-        this.regularCustomerCount = regularCustomerCount;
-        this.visitCount = visitCount;
-        this.isLike = isLike;
-    }
-
-    public MyStoreResponse(Long storeId, String storeName, String thumbnailImage, long regularCustomerCount, int visitCount, boolean isLike) {
-        this.storeId = storeId;
-        this.storeName = storeName;
-        this.thumbnailImage = thumbnailImage;
-        this.regularCustomerCount = regularCustomerCount;
-        this.visitCount = visitCount;
-        this.isLike = isLike;
-    }
-    static public MyStoreResponse of(Long storeId, String storeName, String thumbnailImage, long regularCustomerCount, int visitCount, boolean isLike) {
-        return new MyStoreResponse( storeId,  storeName,  thumbnailImage,  regularCustomerCount,  visitCount,  isLike);
-    }
 }
