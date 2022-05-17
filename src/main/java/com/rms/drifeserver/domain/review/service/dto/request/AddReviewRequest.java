72dd0293284fbc823c4efe79ebe8b1491aefc3a0
pjoon357 <pjoon357@naver.com>
2022-05-17 21:34:12 +0900

    fix: 리뷰 URI에서 storeId 제거

--- src/main/java/com/rms/drifeserver/domain/review/service/dto/request/AddReviewRequest.java
+++ src/main/java/com/rms/drifeserver/domain/review/service/dto/request/AddReviewRequest.java
@@ -1,19 +1,24 @@
 package com.rms.drifeserver.domain.review.service.dto.request;
 
 import com.rms.drifeserver.domain.review.model.Review;
-import com.rms.drifeserver.domain.review.model.ReviewKeywordType;
 import com.rms.drifeserver.domain.store.model.Store;
 import com.rms.drifeserver.domain.user.model.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AddReviewRequest {
 
+    private Long storeId;
+    @Size(max = 300, message = "{review.content.size}")
+    @NotBlank(message = "{review.content.notBlank}")
     private String contents;
 
     private List<Long> keywordIds;
