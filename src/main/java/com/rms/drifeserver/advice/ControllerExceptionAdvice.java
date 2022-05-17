470f4c69f7b4c23fbd63cb3843b2ee4fa0b56a30
pjoon357 <pjoon357@naver.com>
2022-05-17 19:08:53 +0900

    feat: advice에 로깅 추가

--- src/main/java/com/rms/drifeserver/advice/ControllerExceptionAdvice.java
+++ src/main/java/com/rms/drifeserver/advice/ControllerExceptionAdvice.java
@@ -3,6 +3,7 @@ package com.rms.drifeserver.advice;
 import com.rms.drifeserver.domain.common.dto.ApiResponse;
 import com.rms.drifeserver.domain.common.exception.BaseException;
 import com.rms.drifeserver.domain.common.exception.type.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import static com.rms.drifeserver.domain.common.exception.type.ErrorCode.BAD_GATEWAY;
 import static com.rms.drifeserver.domain.common.exception.type.ErrorCode.CONFLICT;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerExceptionAdvice {
 
     @ExceptionHandler(BaseException.class)
     protected ApiResponse<Object> handleBaseException(BaseException exception) {
+        log.error(exception.getMessage(),exception);
         return ApiResponse.error(exception.getErrorCode());
     }
     @ExceptionHandler(Exception.class)
