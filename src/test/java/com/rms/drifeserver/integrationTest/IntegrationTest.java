2fc213eb932e4cb11e78900829e9da282d3409eb
pjoon357 <pjoon357@naver.com>
2022-06-30 19:07:09 +0900

    fix: REVIEW_KEYWORD_TYPE sql문 수정 및 위치 변경

--- src/test/java/com/rms/drifeserver/integrationTest/IntegrationTest.java
+++ src/test/java/com/rms/drifeserver/integrationTest/IntegrationTest.java
@@ -1,12 +1,8 @@
 package com.rms.drifeserver.integrationTest;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
-@Sql(scripts = {"classpath:data/review_keyword_type_insert.sql"})
 public abstract class IntegrationTest {
 
 }
